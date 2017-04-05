df926bd674df7fbd5ac79c0ba47716983fa1dc6c
andriyostryzhniuk <ostryzhnykandriy@gmail.com>
2017-04-05 23:32:13 +0300

    DatabaseConfigFailed deleted

--- src/main/java/andrii/logic/DatabaseConfig.java
+++ src/main/java/andrii/logic/DatabaseConfig.java
@@ -1,22 +1,71 @@
 package andrii.logic;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
 import org.hibernate.SessionFactory;
-import org.hibernate.boot.MetadataSources;
-import org.hibernate.boot.registry.StandardServiceRegistry;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
+import java.util.Properties;
 
-//@org.springframework.context.annotation.Configuration
+@Configuration
+@Component
+@EnableTransactionManagement
 public class DatabaseConfig {
 
-//    @Bean
-    public SessionFactory getSessionFactory() {
-
-        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
-                .configure()
-                .build();
-
-        SessionFactory sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
-        return sessionFactory;
+    public static DataSource dataSource() {
+        return createDataSource("127.0.0.1", "3306", "catering", "root", "qwerty");
     }
+
+    public static DataSource createDataSource(String address,
+                                              String port,
+                                              String dbName,
+                                              String dbUser,
+                                              String dbPassword) {
+        try {
+            ComboPooledDataSource c3p0 = new ComboPooledDataSource();
+            c3p0.setDriverClass("com.mysql.jdbc.Driver");
+            c3p0.setJdbcUrl(String.format("jdbc:mysql://%s:%s/%s", address, port, dbName));
+            c3p0.setUser(dbUser);
+            c3p0.setPassword(dbPassword);
+            return c3p0;
+        } catch (PropertyVetoException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Bean
+    public LocalSessionFactoryBean localSessionFactoryBean() {
+        LocalSessionFactoryBean localSessionFactoryBean = new LocalSessionFactoryBean();
+        localSessionFactoryBean.setDataSource(dataSource());
+        localSessionFactoryBean.setPackagesToScan("andrii");
+        localSessionFactoryBean.setHibernateProperties(hibernateProperties());
+        return localSessionFactoryBean;
+    }
+
+    Properties hibernateProperties() {
+        return new Properties() {
+            {
+                setProperty("hibernate.hbm2ddl.auto", "update");
+                setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+                setProperty("hibernate.show_sql", "true");
+            }
+        };
+    }
+
+    @Bean
+    @Autowired
+    public HibernateTransactionManager transactionManager(SessionFactory sessionFactory) {
+        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
+
+        transactionManager.setSessionFactory(sessionFactory);
+        return transactionManager;
+    }
+
+
 }
