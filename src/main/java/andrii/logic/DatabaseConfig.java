531e84129af7a91b92a542f300f9c5b0f3b71429
andriyostryzhniuk <ostryzhnykandriy@gmail.com>
2017-04-05 17:27:14 +0300

    the second attempt with hibernate.cfg.xml

--- src/main/java/andrii/logic/DatabaseConfig.java
+++ src/main/java/andrii/logic/DatabaseConfig.java
@@ -1,67 +1,22 @@
 package andrii.logic;
 
-import com.mchange.v2.c3p0.ComboPooledDataSource;
 import org.hibernate.SessionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.orm.hibernate5.HibernateTransactionManager;
-import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
-import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
-import javax.sql.DataSource;
-import java.beans.PropertyVetoException;
-import java.util.Properties;
 
-@Configuration
-@Component
-@EnableTransactionManagement
+@org.springframework.context.annotation.Configuration
 public class DatabaseConfig {
 
-    public static DataSource dataSource() {
-        return createDataSource("127.0.0.1", "3306", "catering", "root", "qwerty");
-    }
-
-    public static DataSource createDataSource(String address,
-                                              String port,
-                                              String dbName,
-                                              String dbUser,
-                                              String dbPassword) {
-        try {
-            ComboPooledDataSource c3p0 = new ComboPooledDataSource();
-            c3p0.setDriverClass("com.mysql.jdbc.Driver");
-            c3p0.setJdbcUrl(String.format("jdbc:mysql://%s:%s/%s", address, port, dbName));
-            c3p0.setUser(dbUser);
-            c3p0.setPassword(dbPassword);
-
-            Properties properties = new Properties();
-            properties.setProperty("useSSL", "false");
-            c3p0.setProperties(properties);
-
-            return c3p0;
-        } catch (PropertyVetoException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
     @Bean
-    public LocalSessionFactoryBean sessionFactory () {
+    public SessionFactory getSessionFactory() {
 
-        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
-        sessionFactory.setDataSource(dataSource());
-        sessionFactory.setPackagesToScan(new String[] { "andrii.data.model" });
+        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .configure()
+                .build();
 
+        SessionFactory sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
         return sessionFactory;
     }
-
-    @Bean
-    @Autowired
-    public HibernateTransactionManager transactionManager (SessionFactory sessionFactory) {
-        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
-
-        transactionManager.setSessionFactory(sessionFactory);
-        return transactionManager;
-    }
-
-
 }
---
+++ src/main/java/andrii/logic/DatabaseConfigFaild.java
@@ -0,0 +1,67 @@
+package andrii.logic;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
+import java.util.Properties;
+
+//@Configuration
+//@Component
+//@EnableTransactionManagement
+public class DatabaseConfigFaild {
+
+    public static DataSource dataSource() {
+        return createDataSource("127.0.0.1", "3306", "catering", "root", "qwerty");
+    }
+
+    public static DataSource createDataSource(String address,
+                                              String port,
+                                              String dbName,
+                                              String dbUser,
+                                              String dbPassword) {
+        try {
+            ComboPooledDataSource c3p0 = new ComboPooledDataSource();
+            c3p0.setDriverClass("com.mysql.jdbc.Driver");
+            c3p0.setJdbcUrl(String.format("jdbc:mysql://%s:%s/%s", address, port, dbName));
+            c3p0.setUser(dbUser);
+            c3p0.setPassword(dbPassword);
+
+            Properties properties = new Properties();
+            properties.setProperty("useSSL", "false");
+            c3p0.setProperties(properties);
+
+            return c3p0;
+        } catch (PropertyVetoException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+//    @Bean
+    public LocalSessionFactoryBean sessionFactory () {
+
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource());
+        sessionFactory.setPackagesToScan(new String[] { "andrii.data.model" });
+
+        return sessionFactory;
+    }
+
+//    @Bean
+//    @Autowired
+    public HibernateTransactionManager transactionManager (SessionFactory sessionFactory) {
+        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
+
+        transactionManager.setSessionFactory(sessionFactory);
+        return transactionManager;
+    }
+
+
+}
