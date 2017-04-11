cdc189c063ba65c0e4905b86ae6adb04d63d36e7
andriyostryzhniuk <ostryzhnykandriy@gmail.com>
2017-04-11 16:30:45 +0300

    EncodingFilter deleted

--- src/main/java/andrii/spring/WebInitializer.java
+++ src/main/java/andrii/spring/WebInitializer.java
@@ -1,5 +1,6 @@
 package andrii.spring;
 
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 import javax.servlet.Filter;
@@ -23,7 +24,11 @@ public class WebInitializer extends AbstractAnnotationConfigDispatcherServletIni
 
     @Override
     protected Filter[] getServletFilters() {
-        return new Filter[]{new EncodingFilter()};
+        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+        characterEncodingFilter.setEncoding("UTF-8");
+        characterEncodingFilter.setForceEncoding(true);
+
+        return new Filter[]{characterEncodingFilter};
     }
 
 }
